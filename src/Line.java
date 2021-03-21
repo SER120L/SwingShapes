5463f2895873184f0d4d8698b75fe2c6aa2dd0fe
a-r-t <alexthimineur@gmail.com>
2021-03-21 16:20:29 -0400

    extra line constructor

--- src/Line.java
+++ src/Line.java
@@ -4,4 +4,8 @@ public class Line extends SmartShape {
 	public Line(int x1, int y1, int x2, int y2) {
 		super(new java.awt.geom.Line2D.Double(x1, y1, x2, y2));
 	}
+	
+	public Line(int x1, int y1, int x2, int y2, Color color) {
+		super(new java.awt.geom.Line2D.Double(x1, y1, x2, y2), color);
+	}
 }
