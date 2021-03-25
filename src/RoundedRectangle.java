d0ebdeaf37b058e3e136613494400c524d58f42e
a-r-t <alexthimineur@gmail.com>
2021-03-25 17:55:44 -0400

    fixed roudned rectangle to actually be round

--- src/RoundedRectangle.java
+++ src/RoundedRectangle.java
@@ -1,15 +1,15 @@
 import java.awt.Color;
 
 public class RoundedRectangle extends SmartRectangularShape {
-	public RoundedRectangle() {
-		super(new java.awt.geom.RoundRectangle2D.Double());
+	public RoundedRectangle(int arcSize) {
+		super(new java.awt.geom.RoundRectangle2D.Double(0, 0, 0, 0, arcSize, arcSize));
 	}
 	
-	public RoundedRectangle(int x, int y) {
-		super(new java.awt.geom.RoundRectangle2D.Double(), x, y);
+	public RoundedRectangle(int arcSize, int x, int y) {
+		super(new java.awt.geom.RoundRectangle2D.Double(0, 0, 0, 0, arcSize, arcSize), x, y);
 	}
 	
-	public RoundedRectangle(Color color) {
-		super(new java.awt.geom.RoundRectangle2D.Double(), color);
+	public RoundedRectangle(int arcSize, Color color) {
+		super(new java.awt.geom.RoundRectangle2D.Double(0, 0, 0, 0, arcSize, arcSize), color);
 	}
 }
