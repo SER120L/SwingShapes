4f26f888740dce2397817444959397845e223b72
a-r-t <alexthimineur@gmail.com>
2022-03-30 17:57:50 -0400

    fix line set location

--- src/Line.java
+++ src/Line.java
@@ -3,11 +3,9 @@ import java.awt.Color;
 public class Line extends GenericShape {
 	public Line(int x1, int y1, int x2, int y2) {
 		super(new java.awt.geom.Line2D.Double(x1, y1, x2, y2));
-		super.setLocation(x1, y1);
 	}
 	
 	public Line(int x1, int y1, int x2, int y2, Color color) {
 		super(new java.awt.geom.Line2D.Double(x1, y1, x2, y2), color);
-		super.setLocation(x1, y1);
 	}
 }
